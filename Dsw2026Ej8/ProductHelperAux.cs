0a5cf4ff5a92d0c7d85a902380ff3bf88bb3195b
Maia <maiafedericag@gmail.com>
2026-05-04 18:53:59 -0300

    Problema 1 resuelto

---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        
+        private const string Separador = "-";
+    }
+}
