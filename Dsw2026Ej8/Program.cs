d35f58345eaccf81f7244ed0c36f410cb2bd878f
Maia <maiafedericag@gmail.com>
2026-05-04 22:58:12 -0300

    Problema 6 resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -6,17 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
 
+            Problema6 prob6 = new Problema6();
 
-            Problema5 prob5 = new Problema5();
+            Console.WriteLine("\n--- DEMOSTRACIÓN PROBLEMA 6 ---");
 
-            // Creamos una venta minorista de $1000
-            Sale ventaMinorista = new RetailSale(1000m);
-            // Creamos una venta mayorista de $1000
-            Sale ventaMayorista = new WholesaleSale(1000m);
+            string textoPrueba = "ab 123 x ";
+            Console.WriteLine($"Prueba texto: '{textoPrueba}' -> {prob6.NormalizarCodigoProducto(textoPrueba)}");
 
-            Console.WriteLine("--- DEMOSTRACIÓN PROBLEMA 5 ---");
-            Console.WriteLine($"Total Minorista (esperado 1000): {prob5.ObtenerImporteFinal(ventaMinorista)}");
-            Console.WriteLine($"Total Mayorista (esperado 900): {prob5.ObtenerImporteFinal(ventaMayorista)}");
+            string textoNulo = null;
+            Console.WriteLine($"Prueba null: -> {prob6.NormalizarCodigoProducto(textoNulo)}");
+
         }
     }
 }
